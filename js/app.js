ef8af08a4f8ef608b0ba5cb2bd7716aee1fd10a2
Aileen Wong <aileen.m.wong@gmail.com>
2017-08-05 15:27:48 -0400

    announces winner

--- js/app.js
+++ js/app.js
@@ -8,61 +8,73 @@ window.onload = function() {
     racetrack: document.querySelector('.racetrack')
   }
 
-  var rPosition = 0;
-  var bPosition = 0;
+  let rPosition = 0;
+  let bPosition = 0;
 
   //make a function to move the cars
   function animate(move){
 
-    console.log($els.redCar)
-    console.log($els.blueCar)
-    console.log(move);
+    // console.log($els.redCar)
+    // console.log($els.blueCar)
+    // console.log(move);
+     console.log($els.redCar.style.left)
 
-    //if keyCode 90(z key) is pressed, move the red car 2px
+    //if keyCode 90(z key) is pressed, move the red car 5px
     if (move.keyCode === 90){
-      rPosition += 2;
+      rPosition += 5;
+      //console.log(rPosition)
       $els.redCar.style.left = rPosition + 'px';
-    }
-    //if keyCode 39(right arrow key) is pressed, move the blue car 2px
+      }
+    //if keyCode 39(right arrow key) is pressed, move the blue car 5px
     if (move.keyCode === 39){
-      bPosition += 2;
+      bPosition += 5;
       $els.blueCar.style.left = bPosition + 'px';
     }
+    getWinner();
+  }
+
+    //if one car gets to the end position, that car is the winner
+  function getWinner() {
+    if ($els.redCar.style.left === '445px'){
+      document.querySelector('#result').innerHTML = 'Red Car is the Winner!';
+      //remove event listener
+    }
+    else if ($els.blueCar.style.left === '445px'){
+      document.querySelector('#result').innerHTML = 'Blue Car is the Winner!';
+    }
   }
 
 
-
-  //if one car gets to a certain position, that car is the winner
-  // function getWinner() {
-  //   if (rPosition === ){
-  //     $els.querySelector('#result').innerHTML = 'Red winner'
-  //   }
-  //   else if (bPosition === //finish point)
-  //     return ('Blue Car is the winner')
-  //   //print winner in results box
-
-  // }
+  //grab the start button
+  let startButton = document.querySelector('#beginGame');
+  //when button is clicked, start game
+  startButton.addEventListener('click', start);
 
   //initialize the cars put click event on button, inititalize function animate
-  let button = document.querySelector('button');
-  button.addeventlistener('click', start);
-
   function start() {
+  //
     document.onkeydown = animate;
-  }
+  };
 
-  //reset all cars back to point 0
+
+  //grab the reset button
+  let resetButton = document.querySelector('#resetGame');
+  //when button is clicked, reset game
+  resetButton.addEventListener('click', reset);
+
   function reset() {
-    //reset positions
-    //reset inner html of results
+    //TA told me to do this :)
+    location.reload();
 
-  }
+    // //reset all cars back to point 0
+    // let rPosition = 0;
+    // let bPosition = 0;
+    // //reset results box to blank
+    // $els.querySelector('#result').innerHTML = ' ';
+  };
+}
 
 
-
-
-
-};
 
 
 
