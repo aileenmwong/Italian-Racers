089d3155386cdd48a1599f68d66bd19eaa85cba7
Aileen Wong <aileen.m.wong@gmail.com>
2017-08-05 11:16:32 -0400

    styled CSS but need to fix cars

--- js/app.js
+++ js/app.js
@@ -1,4 +1,34 @@
 window.onload = function() {
-  /* write your code here */
+  console.log('Racers Ready!');
 
+//grab all the DOM elements and wrap them in a container
+const $els = {
+  redCar: document.querySelector('#red'),
+  blueCar: document.querySelector('#blue'),
+  racetrack: document.querySelector('.racetrack'),
+}
+
+//make a function to move the cars
+function animate(move){
+  //if keyCode 90(z key) is pressed, move the red car 2px
+  if (move.keyCode = 90){
+
+
+
+
+  }
+  //if keyCode 39(right arrow key) is pressed, move the blue car 2px
+  if (move.keyCode = 39){
+
+  }
+}
+
+function getWinner() {
+
+}
+})
 };
+
+
+
+
